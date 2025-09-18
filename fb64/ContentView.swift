a146acc546f9318676e5f9e2369cecb9ba212dc6
malinatrash <malinatrashh@gmail.com>
2025-09-18 20:34:30 +0800

    Initial Commit

---
+++ fb64/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fb64
+//
+//  Created by Pavel Naumov on 18.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fb64/fb64App.swift
@@ -0,0 +1,17 @@
+//
+//  fb64App.swift
+//  fb64
+//
+//  Created by Pavel Naumov on 18.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct fb64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
